ff09defe3945fe807c929d99a879cf36f88b595e
luuphuphat <luuphuphat@gmail.com>
2024-01-01 22:48:29 +0700

    create peer connection

--- wecall/src/pages/videocall.jsx
+++ wecall/src/pages/videocall.jsx
@@ -1,16 +1,31 @@
 import React, { useState, useEffect, useRef } from "react";
 
+
+/**
+ * To acquire and communicate streaming data, WebRTC implements the following APIs:
+      MediaStream gets access to data streams, such as from the user's camera and microphone.
+      RTCPeerConnection enables audio or video calling with facilities for encryption and bandwidth management.
+      RTCDataChannel enables peer-to-peer communication of generic data.
+    (There is detailed discussion of the network and signaling aspects of WebRTC later.)
+ */
+
 const VideoCall = () => {
   
   const [localStream, setLocalStream] = useState(null);
   const [remoteStream, setRemoteStream] = useState(null);
+  const [peerConnection, setPeerConnection] = useState(null);
+
   const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
 
   /* camera and micro permission */
   const init = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
       setLocalStream(stream);
+
+      createOffer();
+      
     } catch (error) {
       if (error.name === 'NotFoundError' || error.name === 'DevicesNotFoundError') {
         console.error('Camera not found on the device.');
@@ -20,6 +35,19 @@ const VideoCall = () => {
         console.error('Error accessing media devices:', error);
       }
     }
+  };
+
+  const createOffer = async () => {
+    // Use state to manage peerConnection
+    const newPeerConnection = new RTCPeerConnection();
+    setPeerConnection(newPeerConnection);
+
+    const newRemoteStream = new MediaStream();
+    setRemoteStream(newRemoteStream);
+
+    const offer = await newPeerConnection.createOffer();
+    await newPeerConnection.setLocalDescription(offer);
+    console.log(offer);
   };
 
   useEffect(() => {
@@ -36,7 +64,7 @@ const VideoCall = () => {
   return (
     <div id="videos">
       {localStream ? <video className="video-player" id="user-1" ref={localVideoRef} autoPlay playsInline /> : <video className="video-player" id="user-1" autoPlay playsInline />}
-      <video className="video-player" id="user-2" autoPlay playsInline />
+      {remoteStream ? <video className="video-player" id="user-2" ref={remoteVideoRef} autoPlay playsInline /> : <video className="video-player" id="user-2" autoPlay playsInline />}
     </div>
   );
 };
